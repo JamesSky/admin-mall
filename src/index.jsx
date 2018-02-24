71b377f7c62d47266b91647e7296009d8e71e31f
JamesSky <1511877674@qq.com>
2018-02-24 23:12:50 +0800

    初始化

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import './index.scss';
+import 'font-awesome/css/font-awesome.min.css'
+
+ReactDOM.render(
+	<div>
+		<i className="fa fa-bath"></i>
+		<h1>Hello, world!</h1>,
+	</div>,
+  	document.getElementById('app')
+);
